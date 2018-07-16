5608ee80c700a0761b621841cb95363dc6f479ce
Felipe Lima <flf.felipe@gmail.com>
2018-07-15 23:18:37 -0300

    Adding JUnit test classes

--- src/main/java/br/com/dev/friends/model/Friend.java
+++ src/main/java/br/com/dev/friends/model/Friend.java
@@ -75,4 +75,43 @@ public class Friend implements Serializable {
 		return "Friend [id=" + id + ", name=" + name + ", age=" + age + ", image=" + Arrays.toString(image) + "]";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + Arrays.hashCode(image);
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Friend other = (Friend) obj;
+		if (age != other.age)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (!Arrays.equals(image, other.image))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
+
 }
