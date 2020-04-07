da746dede77a34773c2dbf7845dd9133a4e8f16e
Felipe Lima <flf.felipe@gmail.com>
2020-04-06 23:56:37 -0300

    Adds tests when getting friends

--- src/test/java/br/com/dev/friends/service/FriendServiceTest.java
+++ src/test/java/br/com/dev/friends/service/FriendServiceTest.java
@@ -4,13 +4,62 @@ import br.com.dev.friends.dao.AddressDAO;
 import br.com.dev.friends.dao.FriendDAO;
 import br.com.dev.friends.exception.ResourceNotFoundException;
 import br.com.dev.friends.model.Friend;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 public final class FriendServiceTest {
+
+    private List<Friend> buildFriendsList() {
+        final Friend f1 = new Friend(1L, "John Doe", 20);
+        final Friend f2 = new Friend(2L, "Jane Doe", 30);
+
+        final List<Friend> friends = new ArrayList<>();
+        friends.add(f1);
+        friends.add(f2);
+
+        return friends;
+    }
+
+    private List<Friend> buildEmptyFriendsList() {
+        return new ArrayList<>();
+    }
+
+    @Test
+    void givenASearchWhenTheDBContainsRegistersMustReturnAPopulatedList() {
+        final FriendDAO friendDAO = mock(FriendDAO.class);
+        when(friendDAO.findAll()).thenReturn(buildFriendsList());
+
+        final FriendService friendService = new FriendService(friendDAO, mock(AddressDAO.class));
+
+        final List<Friend> friends = friendService.findAllFriends();
+
+        assertThat(friends).size().isEqualTo(2);
+        assertThat(friends.get(0).getName()).isEqualTo("John Doe");
+        assertThat(friends.get(1).getName()).isEqualTo("Jane Doe");
+
+        verify(friendDAO).findAll();
+    }
+
+    @Test
+    void givenASearchWhenTheDBDoesNotContainsRegistersMustReturnAnEmptyList() {
+        final FriendDAO friendDAO = mock(FriendDAO.class);
+        when(friendDAO.findAll()).thenReturn(buildEmptyFriendsList());
+
+        final FriendService friendService = new FriendService(friendDAO, mock(AddressDAO.class));
+
+        final List<Friend> friends = friendService.findAllFriends();
+
+        assertThat(friends).size().isEqualTo(0);
+
+        verify(friendDAO).findAll();
+    }
 
     @Test
     void givenAnExistentIdMustReturnAFriend() {
@@ -21,12 +70,23 @@ public final class FriendServiceTest {
         try {
             final Friend friend = friendService.findFriendById(1L);
 
-            Assertions.assertThat(friend.getName()).isEqualTo("John Doe");
-            Assertions.assertThat(friend.getAge()).isEqualTo(20);
+            assertThat(friend.getName()).isEqualTo("John Doe");
+            assertThat(friend.getAge()).isEqualTo(20);
 
             verify(friendDAO).findById(1L);
         } catch (ResourceNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    void givenANonExistentIdMustThrowAnException() {
+        final FriendDAO friendDAO = mock(FriendDAO.class);
+        when(friendDAO.findById(any())).thenReturn(null);
+
+        final FriendService friendService = new FriendService(friendDAO, mock(AddressDAO.class));
+        assertThatThrownBy(() -> friendService.findFriendById(1L))
+                .isInstanceOf(ResourceNotFoundException.class)
+                .hasMessage("Friend not found for id 1");
     }
 }
